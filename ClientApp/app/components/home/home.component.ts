8330dc1dcdeac66a4bd9f7c9e462b7d418ab7ed6
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2017-07-07 14:24:49 -0700

    Fix Azure B2C config

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, Inject } from '@angular/core';
 import { IStorage, STORAGE, COOKIES } from "../../config/storage-config";
 import * as MemoryStorage from 'memorystorage';
+import { OidcSecurityService } from "angular-auth-oidc-client";
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    constructor(@Inject(STORAGE) private _storage: IStorage) {}
+    constructor(public oidcSecurityService: OidcSecurityService, @Inject(STORAGE) private _storage: IStorage) {}
 
     getStorage() {
         return JSON.stringify(this._storage);
@@ -15,5 +16,30 @@ export class HomeComponent {
 
     SetACookie() {
         this._storage.setItem("firstcookie","ANOTHER VALUE!");
+    }
+    
+    ngOnInit() {
+        if (window.location.hash) {
+            this.oidcSecurityService.authorizedCallback();
+        }
+    }
+
+    login() {
+        console.log('start login');
+        this.oidcSecurityService.authorize();
+    }
+
+    refreshSession() {
+        console.log('start refreshSession');
+        this.oidcSecurityService.authorize();
+    }
+
+    logout() {
+        console.log('start logoff');
+        this.oidcSecurityService.logoff();
+    }
+
+    checkToken() {
+        console.log(this.oidcSecurityService.getToken());
     }
 }
