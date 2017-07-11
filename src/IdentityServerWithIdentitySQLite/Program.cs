1ff047a55a55e7a6ef2ec2a4e187ef0059dba5cc
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2017-07-11 14:40:33 -0700

    Fixing ports
    Changing server port to match client config.

--- src/IdentityServerWithIdentitySQLite/Program.cs
+++ src/IdentityServerWithIdentitySQLite/Program.cs
@@ -19,7 +19,6 @@ namespace IdentityServerWithAspNetIdentitySqlite
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5050")
                 .Build();
 
             host.Run();
